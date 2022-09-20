c02db82ac32e1bedced9b9b9b1669e0435bfc34b
William <william@zenneatech.com>
2022-09-19 22:11:19 -0700

    Updated profile section, uploaded resources, added layout for site, updated hyperlinks

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,13 @@
 import React from "react";
 import {
   AboutSection,
-  ArticlesSection,
   ContactSection,
   HeroSection,
   InterestsSection,
   Page,
   ProjectsSection,
+  Section,
+  Animation,
   Seo,
 } from "gatsby-theme-portfolio-minimal";
 
@@ -15,12 +16,19 @@ export default function IndexPage() {
     <>
       <Seo title="Gatsby Starter for Portfolio Minimal" />
       <Page useSplashScreenAnimation>
-        <HeroSection sectionId="hero" />
-        <ArticlesSection sectionId="articles" heading="Latest Articles" sources={['Medium']} />
-        <AboutSection sectionId="about" heading="About Portfolio Minimal" />
-        <InterestsSection sectionId="details" heading="Details" />
-        <ProjectsSection sectionId="features" heading="Built-in Features" />
-        <ContactSection sectionId="github" heading="Issues?" />
+          <HeroSection sectionId="Me" />
+        <Animation type="fadeUp">
+          <AboutSection sectionId="About" heading="About Portfolio Minimal" />
+        </Animation>
+        <Animation type="fadeUp">
+                <Section heading="Experiences">
+                    <p>You can add your custom JSX here.</p>
+                    </Section>
+            </Animation>
+        <InterestsSection sectionId="Skills" heading="Skills" />
+        <InterestsSection sectionId="Interests" heading="Interests" />
+        <ProjectsSection sectionId="Projects" heading="Projects" />
+        <ContactSection sectionId="Contact Me" heading="Contact Me" />
       </Page>
     </>
   );
