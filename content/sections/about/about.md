d6472fde589dbc816752554a6e388894387dea26
William <william@zenneatech.com>
2022-09-21 01:09:37 -0700

    Updated experiences section, bugfixes

--- content/sections/about/about.md
+++ content/sections/about/about.md
@@ -7,7 +7,7 @@ Hey There! My name is William and I am a 4th Year student at the University of B
 I'm always keen to learn new things and try new experiences and I think tech is the perfect space for that. I've always loved tech due to the innovation and change it has sparked in the world. As a result, I strive to create to work in tech to contribute to the change I want to see.
 
 Check out everything about me using the top navigation bar! 
-* Experiences: Past Internships & School Involvements
+* Experiences: Past Internships & Extracurricular Involvements
 * Projects: Personal Projects (Check Medium for my paintings!)
 
 Custom keyboards that inspired the color schemes for this website. <a href="https://oblotzky.industries/products/gmk-botanical-2" target="_blank" rel="nofollow noopener noreferrer" aria-label="External Link"><u>GMK Botanical (Light Mode)</u></a>
--- src/pages/experiences.js
+++ src/pages/experiences.js
@@ -1,18 +1,45 @@
 import React from "react";
 import { Page, Seo, Section} from 'gatsby-theme-portfolio-minimal';
 
+function Experience(props) {
+    return (
+    <>
+    <p><h3>{props.position} @ <i>{props.company}</i><img src={props.logo} alt={props.alt} style={{maxHeight: 42, paddingTop: 25}}/></h3> 
+    <div></div>
+    <b>{props.location}—{props.startDate} to{" "}{props.endDate}</b>
+    <div></div>
+    <ul>
+        <li>{props.bullet1}</li>
+        <li>{props.bullet2}</li>
+        <li>{props.bullet3}</li>
+    </ul>
+     <div></div>
+    </p>
+    </>
+    );
+  }
+
 export default function Experiences() {
     return (
         <>
             <Seo title="Experiences" />
             <Page useSplashScreenAnimation>
             <Section sectionId="Work Experiences" heading="Work Experiences">
-            <p>You can add your custom JSX here.</p>
-            </Section>
-            <Section sectionId="Volunteer Experiences" heading="Volunteer Experiences">
-            <p>You can add your custom JSX here.</p>
+            <Experience position="Risk Advisory Analyst" company="" logo="Deloitte_logo.png" alt="Deloitte Logo"
+                        location= "Vancouver, BC" startDate="May 2022" endDate= "Sep 2022"
+                        bullet1= "a" 
+                        bullet2= "b" 
+                        bullet3= "c"
+                        />
+            <h1 style={{paddingTop: 40}}>Extracurricular Involvements</h1>
+            <Experience position="Blueprint Conference Director" company="" logo="Group 274.png" alt="BizTech Logo"
+                        location= "Vancouver, BC" startDate="May 2022" endDate= "Sep 2022"
+                        bullet1= "a" 
+                        bullet2= "b" 
+                        bullet3= "c"
+                        />
             </Section>
             </Page>
         </>
     );
-}
+    }
