a094fdf19e3df82cede8bf223648282e2b3279ae
William <william@zenneatech.com>
2022-09-21 22:43:48 -0700

    Added in experiences section, bugfixes

--- src/pages/experiences.js
+++ src/pages/experiences.js
@@ -4,7 +4,7 @@ import { Page, Seo, Section} from 'gatsby-theme-portfolio-minimal';
 function Experience(props) {
     return (
     <>
-    <p><h3>{props.position} @ <i>{props.company}</i><img src={props.logo} alt={props.alt} style={{maxHeight: 42, paddingTop: 25}}/></h3> 
+    <p><h3>{props.position} @ <i>{props.company}</i></h3> 
     <div></div>
     <b>{props.location}—{props.startDate} to{" "}{props.endDate}</b>
     <div></div>
@@ -14,6 +14,7 @@ function Experience(props) {
         <li>{props.bullet3}</li>
     </ul>
      <div></div>
+     {props.skill1} | {props.skill2} | {props.skill3}
     </p>
     </>
     );
@@ -25,19 +26,52 @@ export default function Experiences() {
             <Seo title="Experiences" />
             <Page useSplashScreenAnimation>
             <Section sectionId="Work Experiences" heading="Work Experiences">
-            <Experience position="Risk Advisory Analyst" company="" logo="Deloitte_logo.png" alt="Deloitte Logo"
+            <Experience position="Risk Advisory Analyst" company="Deloitte" 
                         location= "Vancouver, BC" startDate="May 2022" endDate= "Sep 2022"
                         bullet1= "a" 
                         bullet2= "b" 
                         bullet3= "c"
+                        skill1= "Communication"
+                        skill2= "Teamwork"
+                        skill3= "Consulting"
                         />
+            <Experience position="Android Engineer Intern" company="Zennea Technologies" 
+                        location= "Vancouver, BC" startDate="May 2022" endDate= "Sep 2022"
+                        bullet1= "a" 
+                        bullet2= "b" 
+                        bullet3= "c"
+                        skill1= "Communication"
+                        skill2= "Teamwork"
+                        skill3= "Consulting"
+                        />
+            <Experience position="Risk Advisory Analyst" company="Deloitte" 
+                        location= "Vancouver, BC" startDate="May 2022" endDate= "Sep 2022"
+                        bullet1= "a" 
+                        bullet2= "b" 
+                        bullet3= "c"
+                        skill1= "Communication"
+                        skill2= "Teamwork"
+                        skill3= "Consulting"
+                        />                        
             <h1 style={{paddingTop: 40}}>Extracurricular Involvements</h1>
-            <Experience position="Blueprint Conference Director" company="" logo="Group 274.png" alt="BizTech Logo"
+            <Experience position="Blueprint Conference Director" company="UBC BizTech"
                         location= "Vancouver, BC" startDate="May 2022" endDate= "Sep 2022"
                         bullet1= "a" 
                         bullet2= "b" 
                         bullet3= "c"
+                        skill1= "Communication"
+                        skill2= "Teamwork"
+                        skill3= "Consulting"
                         />
+            <Experience position="Blueprint Conference Director" company="UBC BizTech"
+                        location= "Vancouver, BC" startDate="May 2022" endDate= "Sep 2022"
+                        bullet1= "a" 
+                        bullet2= "b" 
+                        bullet3= "c"
+                        skill1= "Communication"
+                        skill2= "Teamwork"
+                        skill3= "Consulting"
+                        />            
             </Section>
             </Page>
         </>
